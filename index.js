bf3f7412da7f4a5eb549bc08bcd093e6ec61723d
Sung J Byun <sungjbyun@D-10-19-26-239.dhcp4.washington.edu>
2018-05-10 17:39:17 -0700

    basic set up for apollo

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
-const { graphql, buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const { buildSchema } = require('graphql');
+const crypto = require('crypto');
+
+const app = express();
+const PORT = process.env.port || '3000';
 
 const db = {
   users: [
-    { id: '1', email: 'alex@gmail.com', name: 'Alex'},
-    { id: '2', email: 'blex@gmail.com', name: 'Blex'},
-    { id: '3', email: 'clex@gmail.com', name: 'Clex'},
-  ]
+    {
+      id: '1', email: 'alex@gmail.com', name: 'Alex', avatarUrl: 'https://gravatar.com/...',
+    },
+    {
+      id: '2', email: 'blex@gmail.com', name: 'Blex', avatarUrl: 'https://gravatar.com/...',
+    },
+    {
+      id: '3', email: 'clex@gmail.com', name: 'Clex', avatarUrl: 'https://gravatar.com/...',
+    },
+  ],
+  messages: [
+    {
+      id: '1', userId: '1', body: '1ullo!', createdAt: Date.now(),
+    },
+    {
+      id: '2', userId: '2', body: '2ullo!', createdAt: Date.now(),
+    },
+    {
+      id: '3', userId: '1', body: '3ullo!', createdAt: Date.now(),
+    },
+  ],
+};
+
+// Object.assign() method is used to copy the values of all enumerable own properties
+// from one or more source objects to a target object. It will return the target object.
+
+class User {
+  constructor(user) {
+    Object.assign(this, user);
+  }
+  messages() {
+    return db.messages.filter(message => message.userId === this.id);
+  }
 }
 
 const schema = buildSchema(`
   type Query {
     users: [User!]!
+    user(id: ID!): User
+    messages: [Message!]!
+  }
+
+  type Mutation {
+    addUser(email: String!, name: String): User
   }
 
   type User {
@@ -18,27 +60,38 @@ const schema = buildSchema(`
     email: String!
     name: String
     avatarUrl: String
+    messages: [Message!]!
   }
-`)
+
+  type Message {
+    id: ID!
+    body: String!
+    createdAt: String!
+  }
+`);
 
 const rootValue = {
- users: () => db.users
-}
+  users: () => db.users.map(user => new User(user)),
+  user: args => db.users.find(user => user.id === args.id),
+  messages: () => db.messages,
+  addUser: ({ email, name }) => {
+    const user = {
+      id: crypto.randomBytes(10).toString('hex'),
+      email,
+      name,
+    };
+    db.users.push(user);
+    return user;
+  },
+};
 
-graphql(
+
+app.use('/graphql', graphqlHTTP({
   schema,
-  `
-  {
-    users {
-      id
-      email
-      name
-    }
-  }
-  `,
-  rootValue
-).then(
-  res => console.dir(res, {depth: null})
-).catch(
-  console.error
-)
+  rootValue,
+  graphiql: true,
+}));
+
+app.listen(PORT, () => {
+  console.log(`>>>>>  Server running on port: ${PORT}`);
+});
